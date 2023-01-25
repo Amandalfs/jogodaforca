537e5985c2bac341ffeff1f147a20422c452dac2
amandalfs <caduc57@gmail.com>
2023-01-25 14:28:28 -0300

    Version 1.4 Gerar palavra aleatoria de acordo com thema, dar erro se a pessoa nao escolher nada

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 - [x] criar as tags de select para escolher o thema da palavra
 - [x] criar uma lista de dados de palavra de acordo com thema 
 - [ ] ver o layout do site e dar nome para as tags
-- [ ] criar a funcao que pega uma lista de acordo com o thema 
-- [ ] cirar random palavra que gera uma palavra de acordo com thema
+- [x] criar a funcao que pega uma lista de acordo com o thema 
+- [x] cirar random palavra que gera uma palavra de acordo com thema
--- scripts/forca.js
+++ scripts/forca.js
@@ -1,3 +1,36 @@
 import {bancoDeDadosThemeWord} from "./bancoDeDados.js";
 
 console.log(bancoDeDadosThemeWord[0].fruta[80])
+let word = '';
+
+const btnGenerator = document.querySelector('.btnGenerator');
+btnGenerator.addEventListener('click', () => {
+    word = randowWord(word);
+
+})
+
+function randowWord(word){
+    const theme = document.querySelector('.themeSelect').value;
+    const panelGame = document.querySelector('.panel-game');
+    let wordinscriber = document.querySelector('.panel-wcord-insriber');
+    panelGame.style.display = 'none';
+    wordinscriber.innerHTML = '';
+    if(theme==='animal') {
+        panelGame.style.display = 'block';
+        let num = (Math.random()*(bancoDeDadosThemeWord[0].animal.length)).toFixed(0);
+        for(let el of bancoDeDadosThemeWord[0].animal[num]) {
+            wordinscriber.innerHTML += `_ `;
+        }
+        return word = bancoDeDadosThemeWord[0].animal[num];
+    } else if(theme==='fruta') {
+        panelGame.style.display = 'block';
+        let num = (Math.random()*(bancoDeDadosThemeWord[0].fruta.length)).toFixed(0);
+        for(let el of bancoDeDadosThemeWord[0].animal[num]) {
+            wordinscriber.innerHTML += `_ `;
+        }
+        return word = bancoDeDadosThemeWord[0].animal[num];
+    } else {
+        alert("Escolha o tema para iniciar o jogo");
+    }
+    
+}
